53b939d50408e5311296c16770d58f14a6d2feef
User <User@DESKTOP-FGMUVPQ>
2018-04-05 01:57:12 -0400

    YAN TE AMO

--- Chicken_Tracker/src/main/java/br/com/avicultura/chicken_tracker/Servlets/PerfilServlet.java
+++ Chicken_Tracker/src/main/java/br/com/avicultura/chicken_tracker/Servlets/PerfilServlet.java
@@ -20,52 +20,55 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class PerfilServlet extends HttpServlet {
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         Perfil p = Perfil.getInstance();
         p.setNome(request.getParameter("inputNome"));
+        p.setUsuario(request.getParameter("inputUsuario"));
         p.setEmail(request.getParameter("inputEmail"));
         p.setSenha(request.getParameter("inputSenha"));
-        p.setUsuario(request.getParameter("inputLogin"));
+        //p.setFoto(foto);
         HibernateUtil<Perfil> hup = new HibernateUtil<>();
         String s = hup.salvar(p);
         PrintWriter out = response.getWriter();
         out.print(s);
-        response.sendRedirect("main/index.jsp");
+        response.sendRedirect("seusNegocios/negocios.jsp");
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    protected void doPut(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Perfil p = Perfil.getInstance();
+        p.setNome(request.getParameter("inputNome"));
+        p.setUsuario(request.getParameter("inputUsuario"));
+        p.setEmail(request.getParameter("inputEmail"));
+        p.setSenha(request.getParameter("inputSenha"));
+        //p.setFoto(foto);
+        //f.setEstabelecimentos();
+        HibernateUtil<Perfil> hup = new HibernateUtil<>();
+        String s = hup.atualizar(p);
+        PrintWriter out = response.getWriter();
+        out.print(s);
+        response.sendRedirect("seusNegocios/negocios.jsp");
 
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Perfil p = Perfil.getInstance();
+        p.setUsuario(request.getParameter("inputUsuario"));
+        HibernateUtil<Perfil> hup = new HibernateUtil<>();
+        String s = hup.deletar(p);
+        PrintWriter out = response.getWriter();
+        out.print(s);
+        response.sendRedirect("seusNegocios/negocios.jsp");
+    }
 }
--- Chicken_Tracker/src/main/java/br/com/avicultura/chicken_tracker/Servlets/executarLogin.java
+++ Chicken_Tracker/src/main/java/br/com/avicultura/chicken_tracker/Servlets/executarLogin.java
@@ -19,41 +19,8 @@ import javax.servlet.http.HttpSession;
  */
 public class executarLogin extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet executarLogin</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet executarLogin at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+    
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -64,15 +31,7 @@ public class executarLogin extends HttpServlet {
         sessao.invalidate();
         response.sendRedirect("main/index.jsp");
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
