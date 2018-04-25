1bca0cc8bff5d4dfd14e5de50ff64e6fe0a4e68b
Doidao do Capuz <capuz_doidao@github.com>
2018-04-25 13:02:32 -0400

    modals

--- Chicken_Tracker/src/main/webapp/_JS/mascara.js
+++ Chicken_Tracker/src/main/webapp/_JS/mascara.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
     var $seuCampoCpf = $("#inputCPF");
-    $seuCampoCpf.mask('000.000.000-00', {reverse: true});
+    $seuCampoCpf.mask('000.000.000-00', {reverse: false});
     $seuCampoCpf = $("#inputCNPJ");
-    $seuCampoCpf.mask('00.000.000', {reverse: true});
+    $seuCampoCpf.mask('00.000.000', {reverse: false});
     $seuCampoCpf = $("#inputSufixoCNPJ");
-    $seuCampoCpf.mask('0000-00', {reverse: true});
+    $seuCampoCpf.mask('0000-00', {reverse: false});
     $seuCampoCpf = $("#inputRG");
-    $seuCampoCpf.mask('00.000.000-0', {reverse: true});
+    $seuCampoCpf.mask('00.000.000-0', {reverse: false});
     $seuCampoCpf = $("#inputCNAE");
-    $seuCampoCpf.mask('00000-0/00', {reverse: true});
+    $seuCampoCpf.mask('00000-0/00', {reverse: false});
     $seuCampoCpf = $("#inputSalario");
-    $seuCampoCpf.mask('R$ 000000.00', {reverse: true});
+    $seuCampoCpf.mask('R$ 000000.00', {reverse: false});
 });
 
 
