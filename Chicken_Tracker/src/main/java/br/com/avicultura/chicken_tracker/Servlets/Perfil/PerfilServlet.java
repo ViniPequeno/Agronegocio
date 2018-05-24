e11ca770cc19d00d0aa3ececa4c8e73803802858
ViniPequeno <viniciuspedro350@gmail.com>
2018-05-24 10:55:59 -0400

    Merge origin/master
    Conflicts:
    	Chicken_Tracker/src/main/webapp/seusNegocios/estabelecimento.jsp

--- Chicken_Tracker/src/main/java/br/com/avicultura/chicken_tracker/Servlets/Perfil/PerfilServlet.java
+++ Chicken_Tracker/src/main/java/br/com/avicultura/chicken_tracker/Servlets/Perfil/PerfilServlet.java
@@ -8,6 +8,7 @@ package br.com.avicultura.chicken_tracker.Servlets.Perfil;
 import br.com.avicultura.chicken_tracker.Hibernate.HibernateUtil;
 import br.com.avicultura.chicken_tracker.Models.Perfil;
 import br.com.avicultura.chicken_tracker.Utils.Email;
+import br.com.avicultura.chicken_tracker.Utils.Google;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -18,7 +19,11 @@ import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
@@ -40,42 +45,15 @@ public class PerfilServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
-
-//        PrintWriter out = response.getWriter();
-//        Session session = HibernateFactory.getSessionFactory().openSession();
-//        String login = request.getSession().getAttribute("nome_usuario").toString();
-//        Perfil object1 = (Perfil) session.get(Perfil.class, login); // It will get data of which have imageId=1
-//        //byte[] getImageInBytes = object1.getFoto();  // image convert in byte form
-//
-//        File imageFile = new File("myImage.jpg"); // we can put any name of file (just name of new file created).
-//
-//        FileOutputStream outputStream = new FileOutputStream(imageFile); // it will create new file (same location of class)
-//        outputStream.write(getImageInBytes); // image write in "myImage.jpg" file
-//        outputStream.close(); // close the output stream
-//
-//        response.setContentType("text/html");
-//        out = response.getWriter();
-//        out.println(getImageInBytes);
-//        out.println("<html>");
-//        out.println("<head>");
-//        out.println("<title> Cálculo de áreas de figuras geométricas</title>");
-//        out.println("</head>");
-//        out.println("<body>");
-//        out.println(login);
-//        out.print("<br/>");
-//        out.print(object1.getEmail());
-//        out.print("<br/>");
-//        out.println(request.getContextPath());
-//        out.print("<br/>");
-//        out.println(imageFile.getAbsolutePath());
-//        out.print("<br/>");
-//        out.println("Retrieved Image from Database using Hibernate.");
-//        out.print("<br/>");
-//        out.println("<img src='myImage.jpg'>");
-//        out.print("<br/>");
-//        out.println("</body>");
-//        out.println("</html>");
-//        session.close();
+        PrintWriter out = response.getWriter();
+        out.println("Entrou");
+        try {
+            MimeMessage ms = Email.enviar("viniciuspedro350@gmail.com",
+                    "victoryan345@gmail.com", "teste123", "esre123");
+            Email.sendMessage(Google.gmail(), "victoryan345@gmail.com", ms);
+        } catch (Exception ex) {
+            System.out.println("Erro::: "+ex.getMessage());
+        }
     }
 
     @Override
--- Chicken_Tracker/src/main/java/br/com/avicultura/chicken_tracker/Utils/Email.java
+++ Chicken_Tracker/src/main/java/br/com/avicultura/chicken_tracker/Utils/Email.java
@@ -1,18 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.avicultura.chicken_tracker.Utils;
 
+import com.google.api.client.repackaged.org.apache.commons.codec.binary.Base64;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Message;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Properties;
-import javax.mail.Address;
-import javax.mail.Message;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
 
 /**
  *
@@ -20,48 +18,72 @@ import javax.mail.internet.MimeMessage;
  */
 public class Email {
 
-    public String enviar() {
+    public static MimeMessage enviar(String to, String from, String subject, String body) throws AddressException, MessagingException, MessagingException {
         Properties props = new Properties();
-        props.setProperty("mail.smtp.host", "smtp.gmail.com");
-        props.setProperty("mail.smtp.socketFactory.port", "465");
-        props.setProperty("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
-        props.setProperty("mail.smtp.auth", "true");
-        props.setProperty("mail.smtp.port", "587");
-        props.setProperty("mail.smtp.starttls.enable", "true");
-        Session session = Session.getDefaultInstance(props,
-                new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("viniciuspedro350@gmail.com", "Pedro8251");
-            }
-        });
+        Session session = Session.getDefaultInstance(props, null);
+        MimeMessage email = new MimeMessage(session);
+        email.setFrom(new InternetAddress(from));
+        email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
+        email.setSubject(subject);
+        email.setText(body);
+        return email;
+        
+//        props.setProperty("mail.smtp.host", "smtp.gmail.com");
+//        props.setProperty("mail.smtp.socketFactory.port", "465");
+//        props.setProperty("mail.smtp.socketFactory.class",
+//                "javax.net.ssl.SSLSocketFactory");
+//        props.setProperty("mail.smtp.auth", "true");
+//        props.setProperty("mail.smtp.port", "587");
+//        props.setProperty("mail.smtp.starttls.enable", "true");
+//        Session session = Session.getDefaultInstance(props,
+//                new javax.mail.Authenticator() {
+//            protected PasswordAuthentication getPasswordAuthentication() {
+//                return new PasswordAuthentication("viniciuspedro350@gmail.com", "Pedro8251");
+//            }
+//        });
 
         /**
          * Ativa Debug para sessão
-         */
-        session.setDebug(true);
+//         */
+//        session.setDebug(true);
+//
+//        try {
+//
+//            Message message = new MimeMessage(session);
+//            message.setFrom(new InternetAddress("seuemail@gmail.com")); //Remetente
+//
+//            Address[] toUser = InternetAddress //Destinatário(s)
+//                    .parse("viniciuspedro350@gmail.com, victoryan345@gmail.com");
+//
+//            message.setRecipients(Message.RecipientType.TO, toUser);
+//            message.setSubject("Enviando email com JavaMail");//Assunto
+//            message.setText("Enviei este email utilizando JavaMail com minha conta GMail!");
+//            /**
+//             * Método para enviar a mensagem criada
+//             */
+//            Transport.send(message);
+//
+//            return "Oky";
+//
+//        } catch (Exception e) {
+//            return e.getMessage();
+//        }
 
-        try {
-
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("seuemail@gmail.com")); //Remetente
-
-            Address[] toUser = InternetAddress //Destinatário(s)
-                    .parse("viniciuspedro350@gmail.com, victoryan345@gmail.com");
-
-            message.setRecipients(Message.RecipientType.TO, toUser);
-            message.setSubject("Enviando email com JavaMail");//Assunto
-            message.setText("Enviei este email utilizando JavaMail com minha conta GMail!");
-            /**
-             * Método para enviar a mensagem criada
-             */
-            Transport.send(message);
-
-            return "Oky";
-
-        } catch (Exception e) {
-            return e.getMessage();
-        }
-
+    }
+    
+    public static Message createMessage(MimeMessage emailContent) throws IOException, MessagingException{
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        emailContent.writeTo(buffer);
+        byte[] bytes = buffer.toByteArray();
+        String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+        return message;
+    }
+    
+    public static Message sendMessage(Gmail service, String userId, MimeMessage emailContent) throws IOException, MessagingException{
+        Message message = createMessage(emailContent);
+        message = service.users().messages().send(userId, message).execute();
+        return message;
     }
 }
